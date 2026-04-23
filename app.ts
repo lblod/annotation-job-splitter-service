8f67e9876804bf668245170e41c0626b6263fc81
mirdono <alexander.van.den.berghe@redpencil.io>
2026-04-23 08:32:19 +0200

    chore: remove unneeded endpoints
    This service will only react to incoming deltas. There is no need for an
    additional endpoint that can be explicitly called.

--- app.ts
+++ app.ts
@@ -1,16 +1,7 @@
-import { app, query, errorHandler } from "mu";
-
-app.get("/", function (_req, res) {
-  res.send("Hello annotation-job-splitter-service");
-});
+import { app, errorHandler } from "mu";
 
 app.get("/health", async function (_req, res) {
   res.send({ status: "ok" });
-});
-
-app.post("/split-jobs", async function (req, res) {
-  // TODO: Implement
-  res.send("Not implemented yet.");
 });
 
 app.post("/delta", async function (req, res) {
